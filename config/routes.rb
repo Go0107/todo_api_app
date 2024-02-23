064843bce405d633d2feff8be07cefeec033bd28
Go0107 <srfiveu.0107@gmail.com>
2024-02-24 02:02:40 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'todos/index'
   get 'health/health'
   # post "/todos", to: "todos#create"
-  namespace :api do
+  scope :api do
     resources :todos, only: [:index, :create, :update, :destroy]
   end
 end
